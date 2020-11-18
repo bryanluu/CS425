c2ad99f6eb32ad2e0a1e156ba96a8f5da93b869d
Bryan Luu <loc.bryan.luu@gmail.com>
2020-11-17 17:19:44 -0800

    Fixed up indexing bugs and got BFS to work.

--- Graphs.jl
+++ Graphs.jl
@@ -1,6 +1,7 @@
 module Graphs
 
 using LightGraphs, MetaGraphs
+import MetaGraphs
 
 struct Graph{T}
     _idx_map::Dict{T, Int} # Map vertices to indices
@@ -8,22 +9,29 @@ struct Graph{T}
     is_directed::Bool; # whether the graph is directed
     nodetype::DataType; # data type of node
     _metagraph::MetaGraph; # Internal metagraph
-    Graph(nodes::Dict{T, Tuple{N, N}}, directed=false) where {T, N <: Real} = new{T}(
-        Dict(v=>i for (i, v) in enumerate(keys(nodes))), nodes, directed, T,
-        MetaGraph(directed ? SimpleGraph(length(keys(nodes))) : SimpleDiGraph(length(keys(nodes)))));
+
+    function Graph(nodes::Dict{T, Tuple{N, N}}, directed::Bool=false) where {T, N <: Real}
+        idx_map = Dict(v=>i for (i, v) in enumerate(keys(nodes)));
+        pos_map = nodes;
+        mg = MetaGraph(directed ? SimpleGraph(length(keys(nodes))) : SimpleDiGraph(length(keys(nodes))));
+        for (n, i) in idx_map
+            set_prop!(mg, i, :name, n);
+        end
+        new{T}(idx_map, pos_map, directed, T, mg);
+    end
 end
 
 V(g::Graph) = Set(keys(g._idx_map));
 
-nv(g::Graph) = nv(g._metagraph);
+nv(g::Graph) = MetaGraphs.nv(g._metagraph);
 
 function connect!(g::Graph{T}, s::T, d::T, weight::N = 1) where {T, N <: Real}
     mg = g._metagraph;
-    success = add_edge!(mg, s, d);
+    success = add_edge!(mg, g._idx_map[s], g._idx_map[d]);
     if !success
         return false
     else
-        set_prop!(mg, s, d, :weight, weight);
+        set_prop!(mg, g._idx_map[s], g._idx_map[d], :weight, weight);
     end
 end
 
@@ -31,10 +39,55 @@ function connect!(g::Graph{T}, e::Tuple{T, T}, weight::N = 1) where {T, N <: Rea
     connect!(g, e..., weight);
 end
 
-E(g::Graph) = Set(edges(g._metagraph));
+E(g::Graph) = Set([(get_prop(g._metagraph, src(e), :name), get_prop(g._metagraph, dst(e), :name))
+                  for e in edges(g._metagraph)]);
 
-ne(g::Graph) = ne(g._metagraph);
+ne(g::Graph) = MetaGraphs.ne(g._metagraph);
 
-neighbors(g::Graph{T}, v::T) where {T} = neighbors(g, g._idx_map[v]);
+function neighbors(g::Graph{T}, v::T) where {T}
+    [get_prop(g._metagraph, i, :name) for i in MetaGraphs.neighbors(g._metagraph, g._idx_map[v])];
+end
+
+function BFS(g::Graph{T}, s::T, d::T) where {T}
+    if !in(s, keys(g._idx_map)) || !in(d, keys(g._idx_map))
+        return T[];
+    end
+
+    p = Dict{T, Union{T, Nothing}}(v => nothing for v in keys(g._idx_map)); # predecessor vector
+    p[s] = s;
+    Q = T[s];
+
+    function getpath(p::Dict{T, Union{T, Nothing}})
+        curr = d;
+        path = T[];
+        while curr != s
+            pushfirst!(path, curr);
+            curr = p[curr];
+        end
+        pushfirst!(path, s);
+        return path;
+    end
+
+    while !isempty(Q)
+        curr = popfirst!(Q);
+
+        if curr == d
+            return getpath(p);
+        end
+
+        for n in neighbors(g, curr)
+            if p[n] == nothing
+                push!(Q, n);
+                p[n] = curr;
+            end
+
+            if n == d
+                return getpath(p);
+            end
+        end
+
+    end
+
+end
 
 end
